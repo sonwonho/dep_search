25f405324c06e251359a2bff7b0f906cae2f1a12
Ho <sonwonho2005@gmail.com>
2024-04-14 08:38:55 +0900

    ADD url decoding

--- streamlit_front.py
+++ streamlit_front.py
@@ -1,11 +1,13 @@
+import base64
 import streamlit as st
 import pandas as pd
+
 
 df = pd.read_csv('deptCode.csv', encoding = "utf-8")
 tab1, tab2 = st.tabs(["관련 법령", "기관 전화번호"])
 
-results = eval(st.query_params.lawList)
-deptcode_list = eval(st.query_params.deptCodeList)
+results = eval(base64.b64decode(st.query_params.lawList))
+deptcode_list = eval(base64.b64decode(st.query_params.deptCodeList))
 phone_df = pd.DataFrame()
 
 with tab1:
